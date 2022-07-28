f7fcaf4c87e48d9b9f267f385c3e47a6f8c8cb1c
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-07-28 12:49:24 +0530

    Google api key working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-export default function Map() {
+
+import useStyles from './styles.js';
+
+const Map = () => {
+  const isMobile = useMediaQuery('(min-width:600px)');
+  // is Mobile will be true if width less than 600px
+  const classes = useStyles();
+  const coords = {lat:0,lng:0}
+
   return (
-    <div>Map</div>
-  )
-}
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyCXwfULXiz4TIhUIug6EZUET8DH5kEN-2M' }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+      
+      >
+        {/* {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isMobile
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
+        {weatherData?.list?.length && weatherData.list.map((data, i) => (
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+          </div>
+        ))} */}
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
